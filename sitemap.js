5bffe035d6dbe337f1e6ab40762ca3e0cae6bebe
senor <mikasaackerman280105@gmail.com>
2026-03-30 09:21:48 +0700

    push

---
+++ sitemap.js
@@ -0,0 +1,17 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.desagoombongciawi.com/",
+      lastModified: new Date(),
+      priority: 1.0,
+    },
+    {
+      url: "https://www.desagoombongciawi.com/profil",
+      priority: 0.8,
+    },
+    {
+      url: "https://www.desagoombongciawi.com/berita",
+      priority: 0.8,
+    },
+  ];
+}
